65b9ba91fd101a26ca295afc5e7a1564076dc5b3
danremel <remel.dan@gmail.com>
2017-09-09 13:12:35 -0400

    Built out routes and controller for Tasks. Postman renders tasks when GET request is made at 3000/api/tasks/.

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,31 @@
+class Api::TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: @task
+  end
+
+  def create
+    @task = Task.create!(task_params)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+      @task.update(task_params)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    redirect_to api_tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :priority_level, :completion_time)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.destroy_all
+
+Task.create({ title: "Do the dishes", description: "Wash the plates and silverware", priority_level: 2, completion_time: '13:00:00', completed: false })
+Task.create({ title: "Walk the dogs", description: "Make sure they do their business, and clean up after them!", priority_level: 3, completion_time: '17:00:00', completed: false })
