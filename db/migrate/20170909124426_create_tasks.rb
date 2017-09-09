d0a3092734ea112c0196e59ea7b31a1f7fc9e6c8
danremel <remel.dan@gmail.com>
2017-09-09 11:04:08 -0400

    migrated day and task models, installed node-modules for react. got hello world in browser.

--- db/migrate/20170909124426_create_tasks.rb
+++ db/migrate/20170909124426_create_tasks.rb
@@ -5,8 +5,8 @@ class CreateTasks < ActiveRecord::Migration[5.1]
       t.text :description
       t.integer :priority_level
       t.time :completion_time
-      t.completed :boolean
-      
+      t.boolean :completed
+
       t.timestamps
     end
   end
